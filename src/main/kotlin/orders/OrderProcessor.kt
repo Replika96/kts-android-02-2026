eff4d429edb2feb736ead3cce75f2d5d43985cb0
Replika96 <tazmin.vadim31@gmail.com>
2026-02-12 19:02:41 +0300

    Implement Order class with products and status management

--- src/main/kotlin/orders/OrderProcessor.kt
+++ src/main/kotlin/orders/OrderProcessor.kt
@@ -8,6 +8,9 @@ package orders
  *   - Cancelled -> "Order {id} is cancelled: {reason}"
  */
 fun processOrder(order: Order): String {
-    // TODO: use when to return the appropriate string
-    return ""
+    return when(order.status){
+        is OrderStatus.Created -> "Order ${order.id} is new"
+        is OrderStatus.Paid -> "Order ${order.id} is paid"
+        is OrderStatus.Cancelled -> "Order ${order.id} is is cancelled: ${(order.status as OrderStatus.Cancelled).reason}"
+    }
 }
